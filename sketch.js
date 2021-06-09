2ffa05262fc48a5243d2a88a17646c3e07dbfad2
vardhan <bvardhanreddy07@gmail.com>
2021-06-09 13:43:16 +0530

    added constraint

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint =  Matter.Constraint
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,8 +35,13 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(230,180,80, PI/2);
+    
 
     bird = new Bird(100,100);
+   
+    chain = new Chain(bird.body,log6.body);
+    chain2 = new Chain(pig1.body,pig3.body);
 
 }
 
@@ -58,7 +65,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    log6.display();
+    chain.display();
+    chain2.display();
     bird.display();
     platform.display();
 }
